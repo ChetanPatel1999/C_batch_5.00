5b2a273806f8d1feb04d00b92ad94c58ca12582e
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-19 18:03:44 +0530

    first commit

---
+++ 10_comment.c.c
@@ -0,0 +1,15 @@
+//this is comment
+#include<stdio.h>   // stdio.h is a library
+void main()
+{
+	int num;  // here we declaire variable
+	printf("welcome to hello world");
+}
+
+/*
+this is
+multiline 
+comment
+*/
+
+
---
+++ 11_sqaure.c
@@ -0,0 +1,10 @@
+//write a program to take a number from user and display square.
+#include<stdio.h>
+void main()
+{
+  int num,square;
+  printf("enter a num = ");
+  scanf("%d",&num);
+  square= num * num;
+  printf("square = %d",square); 
+}
