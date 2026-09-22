c807178d0044ea989f8c1d06d7f3822dd78555ec
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-22 18:01:25 +0530

    21/09

---
+++ 18_identifire.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int _4age_23 = 90;
+    int marks = 34;
+    int Float = 23;
+    int age;
+    int Age;
+}
---
+++ 19_constant.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    const int passing_marks = 33;
+    const float pi = 3.141;
+    float radius = 3.4;
+    float area;
+    area = pi * radius * radius;
+    printf("area of circle = %.2f", area);
+}
