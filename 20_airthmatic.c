c04974fd72938ac8c4138eb5b8b2bca75ea99675
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-23 18:02:23 +0530

    21/09

---
+++ 20_airthmatic.c
@@ -0,0 +1,11 @@
+//airthmatic operator 
+// +   -   *   /   %
+#include <stdio.h>
+void main()
+{
+    int a, b, res;
+    a = 21;
+    b = 6;
+    res = a % b;
+    printf("res = %d", res);
+}
---
+++ 21_comparision.c
@@ -0,0 +1,11 @@
+// comparision operator
+//  <    >     <=    >=   ==   !-
+#include <stdio.h>
+void main()
+{
+    int a, b, res;
+    a = 45;
+    b = 45;
+    res = a != b;
+    printf("result = %d", res);
+}
